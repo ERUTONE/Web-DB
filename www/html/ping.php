19ee2c438dda119a780c8d348898adda221f1b34
ERUTONE <135013762+ERUTONE@users.noreply.github.com>
2023-12-22 23:12:52 +0900

    Update ping.php

--- www/html/ping.php
+++ www/html/ping.php
@@ -1,6 +1,9 @@
 <?php
-$ip_address = getenv("ip_address");
-$output = shell_exec("ping -c 1 " . $ip_address );
+// pingコマンドを実行するための情報を設定します。
+$ip_address = "192.168.3.83"; // IPアドレスを設定します。
+
+// pingコマンドを実行します。
+$output = shell_exec("ping -c 1 " . $ip_address);
 
 // 結果を表示します。
 echo $output;
