a8efd2a1421690e940f3be00ff53e57241f932eb
ERUTONE <135013762+ERUTONE@users.noreply.github.com>
2023-12-24 00:57:12 +0900

    Update wakeonlan.php

--- www/html/wakeonlan.php
+++ www/html/wakeonlan.php
@@ -1,5 +1,7 @@
 <?php
-$wakeonlan = getenv('wakeonlan');
+$ip_address = getenv('ip_address');
+$mac_address = getenv('mac_address');
+$wakeonlan =("wakeonlan" . $ip_address . $mac_address);
 $output = shell_exec($wakeonlan);
 echo $output;
 ?>
